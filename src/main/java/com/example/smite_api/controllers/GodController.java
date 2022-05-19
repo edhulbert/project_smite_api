fdc6edfbb627d5af012f17bf3a1569940759a4ac
Ed Hulbert <edward.hulbert@hotmail.co.uk>
2022-05-19 13:48:04 +0100

    added God CREATE and DESTROY routes

--- src/main/java/com/example/smite_api/controllers/GodController.java
+++ src/main/java/com/example/smite_api/controllers/GodController.java
@@ -2,14 +2,15 @@ package com.example.smite_api.controllers;
 
 import com.example.smite_api.models.God;
 import com.example.smite_api.repositories.GodRepository;
+import org.aspectj.weaver.patterns.ReferencePointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("gods")
@@ -22,5 +23,30 @@ public class GodController {
     @GetMapping
     public ResponseEntity<List<God>> getGods() {
         return new ResponseEntity<>(godRepository.findAll(), HttpStatus.OK);
+    }
+
+    // SHOW
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<God>> getGodById(@PathVariable Long id) {
+        var god = godRepository.findById(id);
+        return new ResponseEntity<>(god, god.isEmpty() ? HttpStatus.NOT_FOUND : HttpStatus.OK);
+    }
+
+    // CREATE
+    @PostMapping
+    public ResponseEntity<God> createGod(@RequestBody God god) {
+        godRepository.save(god);
+        return new ResponseEntity<>(god, HttpStatus.CREATED);
+    }
+
+    // DESTROY
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> destroyGodById(@PathVariable Long id) {
+        try {
+            godRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
